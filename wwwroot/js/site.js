4e98eace4a4ba73978987f42c2744eadb9662b4d
this0is0kuba <jakub.rakus06@gmail.com>
2023-07-19 13:18:56 +0200

    remove unused code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-//deleteCookie("1 + 1000");
+
 displayAllCookies();
 function setCookie(name) {
     const date = new Date();
